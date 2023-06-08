62bef1c147a0cfe6cf3261caf743d84c4a201388
Egor <azazaz.com534@gmail.com>
2023-06-08 12:01:02 +0300

    hw

--- src/main/java/com/example/shoppingbasket/BasketServiceImpl.java
+++ src/main/java/com/example/shoppingbasket/BasketServiceImpl.java
@@ -2,8 +2,6 @@ package com.example.shoppingbasket;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
